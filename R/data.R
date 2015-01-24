e3143a6e51a5d408c192a91cf4b44e0272b92e4e
Kenny Darrell <darrelkj@gmail.com>
2015-01-24 11:03:18 -0500

    move large data out

--- R/data.R
+++ R/data.R
@@ -5,18 +5,3 @@
 #' @usage data(birthPS)
 NULL
 
-
-#' @name birthUS1
-#' @title birthUS1
-#' @description The Nat2013PublicUS.r20141016 birth data from cdc.gov part 1 of 2
-#' @docType data
-#' @usage data(birthUS1)
-NULL
-
-
-#' @name birthUS2
-#' @title birthUS2
-#' @description The Nat2013PublicUS.r20141016 birth data from cdc.gov part 2 of 2
-#' @docType data
-#' @usage data(birthUS2)
-NULL
