e3be65876e9cf470c8792420a8321d6013b717b4
Kenny Darrell <darrelkj@gmail.com>
2015-01-24 10:41:04 -0500

    data lazy load code

---
+++ R/data.R
@@ -0,0 +1,22 @@
+#' @name birthPS
+#' @title birthPS
+#' @description The Nat2013PublicPS.r20141016 birth data from cdc.gov
+#' @docType data
+#' @usage data(birthPS)
+NULL
+
+
+#' @name birthUS1
+#' @title birthUS1
+#' @description The Nat2013PublicUS.r20141016 birth data from cdc.gov part 1 of 2
+#' @docType data
+#' @usage data(birthUS1)
+NULL
+
+
+#' @name birthUS2
+#' @title birthUS2
+#' @description The Nat2013PublicUS.r20141016 birth data from cdc.gov part 2 of 2
+#' @docType data
+#' @usage data(birthUS2)
+NULL
